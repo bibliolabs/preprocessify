dbcab5cc9d737e3c2dd287d60caea706c0445895
c4605 <bolasblack@gmail.com>
2015-08-19 13:47:13 +0800

    upgrade dependencies

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var transformTools = require('browserify-transform-tools');
 var pp = require('preprocess');
 
 module.exports = function(preprocessContext, options) {
-    options = options || {includeExtensions: [".js"]};
+    options = options || {includeExtensions: [".js"], type: 'js'};
     return transformTools.makeStringTransform("preprocessify", options,
             function (src, transformOptions, done) {
-                done(null, pp.preprocess(src, preprocessContext, 'js'));
+                done(null, pp.preprocess(src, preprocessContext, options));
             });
 };
