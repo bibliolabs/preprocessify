fd38283c55c5b3d0cf3a4e62fa602f5431d15cf3
Evan Muller <evanmuller@gmail.com>
2015-08-21 16:14:05 -0400

    Fixed default options not set correctly

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ var transformTools = require('browserify-transform-tools');
 var pp = require('preprocess');
 
 module.exports = function(preprocessContext, options) {
-    options = options || {includeExtensions: [".js"], type: 'js'};
+    options = options || {};
+
+    if (!options.includeExtensions) {
+        options.includeExtensions = [".js"];
+    }
+
+    if (!options.type) {
+        options.type = 'js';
+    }
+
     return transformTools.makeStringTransform("preprocessify", options,
             function (src, transformOptions, done) {
                 done(null, pp.preprocess(src, preprocessContext, options));
